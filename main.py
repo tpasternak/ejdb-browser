8e56523e8651bada8c653059c7b36858e430482d
Patryk Ławski <patryk.lawski@gmail.com>
2016-04-02 18:26:58 +0200

    treeview introduction

--- main.py
+++ main.py
@@ -2,8 +2,26 @@ from kivy.app import App
 from kivy.uix.widget import Widget
 import pyejdb
 from kivy.properties import StringProperty
+
+from kivy.uix.treeview import TreeViewLabel
+
+
 class Browser(Widget):
     ludzie=StringProperty("")
+
+    def callback(treeview,a,b):
+
+        with pyejdb.EJDB("city", pyejdb.DEFAULT_OPEN_MODE) as db:
+            cursor = db.find("people")
+            items = []
+            for entry in cursor:
+                entryString = ""
+                for fields in entry.items():
+                    entryString += fields[1] + " "
+                items.append(entryString)
+
+            for name in items:
+                yield TreeViewLabel(text=name)
 
     pass
 
@@ -11,13 +29,14 @@ class Browser(Widget):
 class EjdbBrowser(App):
     def build(self):
         browser = Browser()
-        db = pyejdb.EJDB("city", pyejdb.DEFAULT_OPEN_MODE)
-        cursor = db.find("people")
-        for entry in cursor:
-            browser.ludzie += "=======================\n"
-            for fields in entry.items():
-                browser.ludzie += ("{0}:{1}".format(fields[0], fields[1])) + "\n"
-        print(browser.ludzie)
+#        db = pyejdb.EJDB("city", pyejdb.DEFAULT_OPEN_MODE)
+#        cursor = db.find("people")
+
+#        for entry in cursor:
+#            browser.ludzie += "=======================\n"
+#            for fields in entry.items():
+#                browser.ludzie += ("{0}:{1}".format(fields[0], fields[1])) + "\n"
+#        print(browser.ludzie)
 
         return browser
 
