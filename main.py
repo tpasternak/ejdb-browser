dfa0b24636f13078522f84d771eab7cf33d27b2f
Tymek Chmielecki <tymek@Tymeks-MacBook-Pro.local>
2016-04-03 11:55:03 +0200

    Added a popup window which lets you select a file but doesn't set the path

--- main.py
+++ main.py
@@ -7,16 +7,50 @@ from kivy.uix.button import Button
 from kivy.properties import StringProperty
 from kivy.uix.treeview import TreeViewLabel
 from kivy.uix.filechooser import FileChooserController
+from kivy.factory import Factory
+from kivy.properties import ObjectProperty
+from kivy.uix.popup import Popup
 
+import os
 
 import pyejdb
 
+class LoadDialog(FloatLayout):
+    load = ObjectProperty(None)
+    cancel = ObjectProperty(None)
+
+
+class SaveDialog(FloatLayout):
+    save = ObjectProperty(None)
+    text_input = ObjectProperty(None)
+    cancel = ObjectProperty(None)
+
+
+class Root(FloatLayout):
+
+    loadfile = ObjectProperty(None)
+    text_input = ObjectProperty(None)
+
 
 class Browser(Widget):
-    people=StringProperty("")
+    people = StringProperty("")
     layout = GridLayout()
+    path1 = ''
+    already_created = False
 
-    already_created = False	
+    def show_load(self):
+        content = LoadDialog(load=self.load, cancel=self.dismiss_popup)
+        self._popup = Popup(title="Load file", content=content,
+                            size_hint=(0.9, 0.9))
+        self._popup.open()
+
+    def load(self, path, filename):
+        path1=path
+        self._popup.dismiss()
+
+    def dismiss_popup(self):
+        self._popup.dismiss()
+
     def callback(self,treeview,a):
         with pyejdb.EJDB("city", pyejdb.DEFAULT_OPEN_MODE) as db:
             cursor = db.find("people")
@@ -33,16 +67,18 @@ class Browser(Widget):
 
             self.already_created = True
 
-    def callback1(instance):
-        print('deunisc')
-
+    def callback1(self):
+        self.show_load()
     pass
+
 
 class EjdbBrowser(App):
     def build(self):
         browser = Browser()
-
         return browser
+    Factory.register('Root', cls=Root)
+    Factory.register('LoadDialog', cls=LoadDialog)
+
 
 if __name__ == '__main__':
     EjdbBrowser().run()
